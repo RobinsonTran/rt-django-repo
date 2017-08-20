ceaaac58a61e7e00c33ee64285e70df4f19c05fa
RT_Prod <RT_Prod@Robinsons-MacBook-Pro-3.local>
2017-08-20 17:06:09 -0400

    more conditionals

--- py_tut_1.py
+++ py_tut_1.py
@@ -436,3 +436,21 @@ lbreak()
 basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
 for f in sorted(set(basket)):
     print(f)
+
+lbreak()
+
+import math
+raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('Nan'), 47.8]
+filtered_data = []
+for value in raw_data:
+    if not math.isnan(value):
+        filtered_data.append(value)
+print filtered_data
+
+lbreak()
+
+#--------------------------More on Conditions----------------------------
+
+string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
+non_null = string1 or string2 or string3
+print non_null
