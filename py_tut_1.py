344a86b8ccc5f2ccc4a1c1a56a52bec5079b74b5
Robinson Tran <robinson.c.tran@gmail.com>
2017-10-10 21:21:08 -0400

    laptop push

--- py_tut_1.py
+++ py_tut_1.py
@@ -531,8 +531,42 @@ def lbreak():
 #     for line in f:
 #         print(line, end="")
 
-#----------------------------Classes--------------------------------------
+#-----------------------------------Classes--------------------------------------
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
 
 
+lbreak()
+
+class MyClass:
+    """A simple example class"""
+    i = 123456
+
+    def f(self):
+        return 'hello world'
+
+x = MyClass()
+
+def __init__(self):
+    self.data = []
 
 
